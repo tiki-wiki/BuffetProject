e2de170832885ba394ca48c8837bae65a9c2674d
Nima Hosseini <Tiki.Wiki@hotmail.com>
2020-05-20 22:17:22 +0430

    add state

---
+++ Main Project/BuffetAssistant/Order.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuffetAssistant.Classes
+{
+    public enum Status
+    {
+        Ready,
+        Unready,
+        Cancle
+    }
+    class Order
+    {
+        private int id;
+        private string name;
+        private double totalPricel;
+        private int quantity;
+        private Status state;
+        public int ID
+        {
+            get { return id;}
+            set { id = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+}
