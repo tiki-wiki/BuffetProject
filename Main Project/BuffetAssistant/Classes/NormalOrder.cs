4dd1a645432d7d0f1d52ca438c21d10e2dbc6f3e
Nima Hosseini <Tiki.Wiki@hotmail.com>
2020-05-25 15:20:28 +0430

    add lots of codes

--- Main Project/BuffetAssistant/Classes/NormalOrder.cs
+++ Main Project/BuffetAssistant/Classes/NormalOrder.cs
@@ -8,5 +8,27 @@ namespace BuffetAssistant.Classes
 {
     class NormalOrder:Order
     {
+        //fileds
+        private int id;
+        private string name;
+        private double totalPrice;
+        private Status state;
+        private int quantity;
+        public NormalOrder(int id, string name, double totalPrice, Status state, int quantity)
+        {
+            this.id = id;
+            this.name = name;
+            this.totalPrice = totalPrice;
+            this.state = state;
+            this.quantity = quantity;
+        }
+        public NormalOrder()
+        {
+            this.id = 0;
+            this.name = "No Name";
+            this.totalPrice = 0;
+            this.state = Status.Unready;
+            this.quantity = 1;
+        }
     }
 }
--- Main Project/BuffetAssistant/Classes/Order.cs
+++ Main Project/BuffetAssistant/Classes/Order.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Reflection;
 
 namespace BuffetAssistant.Classes
 {
@@ -65,7 +67,11 @@ namespace BuffetAssistant.Classes
         }
         public Order()
         {
-
+            this.id = 0;
+            this.name = "No Name";
+            this.totalPrice = 0;
+            this.state = Status.Unready;
+            this.quantity = 1;
         }
     }
 }
