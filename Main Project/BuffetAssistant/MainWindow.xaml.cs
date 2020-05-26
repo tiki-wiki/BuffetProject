4ebf4332be1200ff4d8db2e1682f06973e67f3a4
Nima Hosseini <Tiki.Wiki@hotmail.com>
2020-05-26 20:11:48 +0430

    delete all codes

--- Main Project/BuffetAssistant/MainWindow.xaml.cs
+++ Main Project/BuffetAssistant/MainWindow.xaml.cs
@@ -25,35 +25,6 @@ namespace BuffetAssistant
         public MainWindow()
         {
             InitializeComponent();
-            o1 = new Order();
-            o1.ID = 4;
-            o1.Name = "Hamburger";
-            o1.Quantity = 50;
-            o1.TotalPrice = 2500;
-            hamInfo.Text = "Id:" + o1.ID.ToString() + "\n" + "Name:" + o1.Name + "\n" + "Quantity:" +
-                o1.Quantity.ToString() + "\n" + "TotalPrice: " + o1.TotalPrice.ToString() + "$";
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            if (EXP.IsExpanded == true)
-            {
-                orderListBox.Items.Add("Order 1");
-                orderListBox.Items.Add(o1.ID);
-                orderListBox.Items.Add(o1.Name);
-                orderListBox.Items.Add(o1.Quantity);
-                orderListBox.Items.Add(o1.TotalPrice);
-                EXP.IsExpanded = false;
-            }
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-        
-            if (orderListBox.SelectedItems.Count!=0)
-            {
-                orderListBox.Items.Remove(orderListBox.SelectedItem);
-            }
         }
     }
 }
