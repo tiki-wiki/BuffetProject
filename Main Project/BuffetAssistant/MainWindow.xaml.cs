19d7aedf8e3d724536f2f35e1ba31eb2e66dc2ed
Nima Hosseini <Tiki.Wiki@hotmail.com>
2020-05-27 20:33:22 +0430

    change buttons

--- Main Project/BuffetAssistant/MainWindow.xaml.cs
+++ Main Project/BuffetAssistant/MainWindow.xaml.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -28,6 +29,11 @@ namespace BuffetAssistant
         private void CloseCommandHandler(object sender, ExecutedRoutedEventArgs e)
         {
             Application.Current.Shutdown();
+        }
+
+        private void Drag(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
         }
     }
 }
