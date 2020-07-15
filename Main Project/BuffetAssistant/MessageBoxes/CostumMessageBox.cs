d595d2c0abbebe33681e1b6ca94f6b5527e73c6b
Nima Hosseini <Tiki.Wiki@hotmail.com>
2020-07-15 21:13:51 +0430

    Last Change

--- Main Project/BuffetAssistant/MessageBoxes/CostumMessageBox.cs
+++ Main Project/BuffetAssistant/MessageBoxes/CostumMessageBox.cs
@@ -19,19 +19,31 @@ namespace BuffetAssistant.MessageBoxes
         }
         public enum MessageIcons
         {
-
+            Error,
+            Exclamation,
+            Information,
+            Question,
+            Warning
         }
-        public static string Show(string text)
+        public static int ShowDialog()
         {
-            return Show(text);
+            return -1;
+        }
+        public static int Show(string text)
+        {
+            return -1;
         }
         public static string Show(string text, string caption)
         {
-            return Show(text, caption);
+            return Show(text,caption);
         }
         public static string Show(string text, string caption, MessageButtons messageButtons)
         {
             return Show(text, caption,messageButtons);
+        }
+        public static string Show(string text, string caption, MessageButtons messageButtons,MessageIcons messageIcon)
+        {
+            return Show(text, caption, messageButtons,messageIcon);
         }
     }
 }
