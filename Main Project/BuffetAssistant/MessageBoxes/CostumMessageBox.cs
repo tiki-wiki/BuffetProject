4d5e23c6c97d062e9579e7940cc46b7f9bc86a1e
Nima Hosseini <Tiki.Wiki@hotmail.com>
2020-07-15 19:21:23 +0430

    add messageBox Class

---
+++ Main Project/BuffetAssistant/MessageBoxes/CostumMessageBox.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace BuffetAssistant.MessageBoxes
+{
+    class CostumMessageBox
+    {
+        public enum Buttons
+        {
+            Yes,
+            No,
+            Cancel,
+            Retry,
+            OK
+        }
+        public static string Show(string text,string Caption)
+        {
+            return Show(text, Buttons.OK);
+        }
+    }
+}
