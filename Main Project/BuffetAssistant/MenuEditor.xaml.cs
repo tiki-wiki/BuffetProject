ce73e4973531cebec04b19aae2d01b6523ab659b
Nima Hosseini <Tiki.Wiki@hotmail.com>
2020-08-04 19:47:44 +0430

    working on editor

--- Main Project/BuffetAssistant/MenuEditor.xaml.cs
+++ Main Project/BuffetAssistant/MenuEditor.xaml.cs
@@ -11,7 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-
+using Microsoft.Win32;
 namespace BuffetAssistant
 {
     /// <summary>
@@ -22,6 +22,22 @@ namespace BuffetAssistant
         public MenuEditor()
         {
             InitializeComponent();
+        }
+        public static ImageSource GetImageSource(string path)
+        {
+            BitmapImage iconSource = new BitmapImage();
+            iconSource.BeginInit();
+            iconSource.UriSource = new Uri(path);
+            iconSource.EndInit();
+            return iconSource;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog imageFile = new OpenFileDialog();
+            imageFile.Filter = "Image Files (*.jpg)|*.jpg|(*.png)|*.png|(*.ico)|*.ico";
+            if (imageFile.ShowDialog() == true)
+                foodImage.Source = GetImageSource(imageFile.FileName);
         }
     }
 }
